dafef162de3fba771c021e3d6b0beb2737e11b12
Rudolphish <rudi.rednose.wolf@gmail.com>
2023-09-26 00:12:16 +0900

    homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import BlogPage from "./Components/Pages/BlogPage";
+import Homepage from "./Components/Pages/Homepage";
 import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
-  return(
-    <div className="App">
-      <Header />
+  return (
+    <Router>
+      <div className="App">
+        <Header />
 
-    </div>
-  )
-
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/blog" element={<BlogPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -10,13 +11,13 @@ const Header = () => {
       <nav>
           <ul>
               <li>
-                  <a href="##">HOME</a>
+                  <Link to="/">HOME</Link>
               </li>
               <li>
-                  <a href="##">BLOG</a>
+                  <Link to="/blog">BLOG</Link>
               </li>
               <li>
-                  <a href="##">SNS</a>
+                  <a href="/sns">SNS</a>
               </li>
           </ul>
       </nav>
