6b9f9033801116da47b8231ae48a32ab2d9d7bb3
Rudolphish <rudi.rednose.wolf@gmail.com>
2023-09-25 21:50:16 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>WELCOME!!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <a
-            className="App-link"
-            href="https://main.dtulr0y5rojd6.amplifyapp.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Check_AWS_WebSite!!
-          </a>
-        </div>
-      </header>
+      <Header />
+
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+  <header>
+      <div>
+         <h3>My First Portfolio</h3> 
+      </div>
+
+      <nav>
+          <ul>
+              <li>
+                  <a href="##">HOME</a>
+              </li>
+              <li>
+                  <a href="##">BLOG</a>
+              </li>
+              <li>
+                  <a href="##">SNS</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
+  );
+};
+
+export default Header;
